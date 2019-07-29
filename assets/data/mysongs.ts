edf68e5ae2ff10dbb76ea555e216db8ebf88c6aa
JainSheridan <50390799+JainSheridan@users.noreply.github.com>
2019-07-29 15:24:43 -0400

    Add files via upload

---
+++ assets/data/mysongs.ts
@@ -0,0 +1,10 @@
+import {songInfo} from  '../../app/songInfo';
+
+
+export const Songs : songInfo[] = [
+
+    { song_name : 'Shape of You',artist : 'Ed Sheeran',genere : 'Pop',year : 2017,picture : 'shapeofyou.jpeg'},
+    { song_name : 'Mirzapur-Sultan',artist : 'Sultan feat. OG Ghuman',genere : 'Hip/Hop/Rap',year : 2019,picture : 'sultan.jpg'},
+    { song_name : 'The Wakhra Song',artist : 'Navv Inder',genere : 'Rap',year : 2015,picture : 'wakhra.jpg'},
+    { song_name : 'Teri Aakhya Ka Yo Kajal',artist : 'DC Madana',genere : 'Romance',year : 2018,picture : 'kajal.jpeg'}
+];
